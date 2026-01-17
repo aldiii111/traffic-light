16f581ac66d7af7f6cacf3e70ec8d97ceef7f068
aldiii <aldinissa093@gmail.com>
2026-01-17 13:42:41 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let red = document.getElementById('redLight')
+let yellow = document.getElementById('yellowLight')
+let green = document.getElementById('greenLight')
+
+function nyala() {
+    red.className = "bg-gray-700 w-20 h-20 rounded-full"
+}
