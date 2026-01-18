c686ba1c9c4cd5e42b57b8f2ce88b16fc2c73711
aldiii <aldinissa093@gmail.com>
2026-01-19 05:55:39 +0700

    finish pake ai dikit

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-let red = document.getElementById('redLight')
-let yellow = document.getElementById('yellowLight')
-let green = document.getElementById('greenLight')
+const red = document.getElementById('redLight');
+const yellow = document.getElementById('yellowLight');
+const green = document.getElementById('greenLight');
 
-function nyala() {
-    red.className = "bg-gray-700 w-20 h-20 rounded-full"
+const baseLamp = "w-20 h-20 rounded-full ";
+const offLamp = baseLamp + "bg-gray-700 ";
+const redOn = offLamp + "bg-red-400 ";
+const yellowOn = offLamp + "bg-yellow-400 ";
+const greenOn = offLamp + "bg-green-400 ";
+
+function turnOff() {
+    red.className = offLamp;
+    yellow.className = offLamp;
+    green.className = offLamp;
 }
+
+function turnOn() {
+    turnOff();
+    red.className = redOn;
+    setTimeout(() => {
+        turnOff();
+        green.className = greenOn;
+        setTimeout(() => {
+            turnOff();
+            yellow.className = yellowOn;
+            setTimeout(turnOn, 2000)
+        }, 4000)
+    }, 5000);
+}
+
+turnOn();
